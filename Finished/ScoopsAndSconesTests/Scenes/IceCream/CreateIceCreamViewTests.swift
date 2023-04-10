89c76e1c0b539477a76bab027ed177cadde59ce4
alex99091 <bkk99091@gmail.com>
2023-04-10 20:18:10 +0900

    complete Tests

--- Finished/ScoopsAndSconesTests/Scenes/IceCream/CreateIceCreamViewTests.swift
+++ Finished/ScoopsAndSconesTests/Scenes/IceCream/CreateIceCreamViewTests.swift
@@ -38,18 +38,35 @@ class CreateIceCreamViewTests: XCTestCase {
   var sut: CreateIceCreamView!
   var interactorSpy: CreateIceCreamInteractorSpy!
   // swiftlint:enable implicitly_unwrapped_optional
-
+  
   override func setUpWithError() throws {
     try super.setUpWithError()
     sut = CreateIceCreamView()
     interactorSpy = CreateIceCreamInteractorSpy()
   }
-
+  
   // MARK: - Test doubles
-  class CreateIceCreamInteractorSpy: CreateIceCreamBusinessLogic {}
-
+  class CreateIceCreamInteractorSpy: CreateIceCreamBusinessLogic {
+    var loadIceCreamCalled = false
+    
+    func loadIceCream(request: CreateIceCream.LoadIceCream.Request) {
+      loadIceCreamCalled = true
+    }
+  }
+  
   // MARK: - Tests
-
+  func testShouldLoadIceCreamOnViewAppear() {
+    // Given
+    sut.interactor = interactorSpy
+    // When
+    sut.fetchIceCream()
+    // Then
+    XCTAssertTrue(
+      interactorSpy.loadIceCreamCalled,
+      "fetchIceCream() should ask the interactor to load the ice cream"
+    )
+  }
+  
   override func tearDownWithError() throws {
     sut = nil
     interactorSpy = nil
